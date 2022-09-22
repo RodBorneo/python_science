108980a9f551cc36727530100c96b47e51e80fc2
RodBorneo <borneorodrigo@yahoo.com.br>
2022-09-21 23:48:30 -0300

    4_ED 4.5 e 4.6

---
+++ lista_lambda.py
@@ -0,0 +1,15 @@
+#Utilizando somente uma linha de programação, crie uma lista que contenha 
+#os números ímpares de 1 a 51.
+
+
+#A função range() tem como padrão 0 como valor inicial, 
+#porém é possível especificar o valor inicial adicionando um parâmetro: 
+#range(2, 6), que significa valores de 2 a 6 (mas não incluindo 6):
+
+lista_impares = lista_impares = [(i*2 + 1) for i in range (26)]
+print(lista_impares)
+
+#lambda x: função_x 	Retorna uma função
+f_impares = lambda x: (x*2+1)	
+lista_impares_V2 = [f_impares(n) for n in range(26)]
+print(lista_impares_V2)
---
+++ loop_remocao.py
@@ -0,0 +1,14 @@
+#Dada a lista ['P','A','Y','A','T','A','H','O','N'], conte o número de variáveis 'A' 
+#e utilize um loop para remover todos os 'A' excedente.
+
+Lista = ['P','A','Y','A','T','A','H','O','N']
+print(Lista)
+
+count_A = 0
+for x in Lista:
+  if x == 'A':
+    count_A += 1
+    Lista.remove(x)
+    
+print(count_A)
+print(Lista) 

a4d8607e6a5dd3499c76d40548fd934d312ad7b4
RodBorneo <borneorodrigo@yahoo.com.br>
2022-09-23 22:44:39 -0300

    usando lambda para criar lista

--- lista_lambda.py
+++ lista_lambda.py
@@ -1,11 +1,12 @@
-#Utilizando somente uma linha de programação, crie uma lista que contenha 
-#os números ímpares de 1 a 51.
-
-
-#A função range() tem como padrão 0 como valor inicial, 
-#porém é possível especificar o valor inicial adicionando um parâmetro: 
-#range(2, 6), que significa valores de 2 a 6 (mas não incluindo 6):
-
+"""
+Utilizando somente uma linha de programação, crie uma lista que contenha 
+os números ímpares de 1 a 51.
+"""
+"""
+A função range() tem como padrão 0 como valor inicial, 
+porém é possível especificar o valor inicial adicionando um parâmetro: 
+range(2, 6), que significa valores de 2 a 6 (mas não incluindo 6):
+"""
 lista_impares = lista_impares = [(i*2 + 1) for i in range (26)]
 print(lista_impares)
 
